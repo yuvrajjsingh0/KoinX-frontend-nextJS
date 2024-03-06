167409cd8d0abfe2ec1213ba9a0d82e465640f7e
Yuvraj Singh <yuvrajjsingh0@gmail.com>
2024-03-07 04:23:31 +0530

    Changed slug in breadcrumb and made /bitcoin as default page

--- src/app/[coin]/layout.tsx
+++ src/app/[coin]/layout.tsx
@@ -4,6 +4,7 @@ import Header from "@/components/common/Header"
 import RightAdv from "@/components/common/RightAdv";
 import CurrencyDataProvider from "@/util/context/CurrencyDataProvider";
 import TrendingCoins from "@/components/coin/trendingcoins";
+import { usePathname } from "next/navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,8 +13,9 @@ export const metadata = {
   description: "CryptoCurrency is Fun!",
 };
 
-export default function RootLayout({ children }: Readonly<{
+export default function RootLayout({ children, params }: Readonly<{
     children: React.ReactNode;
+    params: any;
 }>) {
   return (
     <html lang="en" style={{scrollBehavior:'smooth'}}>
@@ -23,7 +25,7 @@ export default function RootLayout({ children }: Readonly<{
       <body className={inter.className}>
         <Header/>
         <div className="p-4 ml-[11px] md:ml-[40px] ">
-          Cryptocurrencies {'>>'} Bitcoin
+          Cryptocurrencies {'>>'} {params.coin.charAt(0).toUpperCase() + params.coin.slice(1)}
         </div>
         <CurrencyDataProvider>
           <div className="flex flex-col px-[16px] xl:flex-row xl:px-[57px] lg:gap-[20px] ">
---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Koinx",
+  description: "CryptoCurrency is Fun!",
+};
+
+export default function RootLayout({ children }: Readonly<{
+    children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" style={{scrollBehavior:'smooth'}}>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
+      <body className={inter.className}>
+        {children}
+      </body>
+    </html>
+  );
+}
